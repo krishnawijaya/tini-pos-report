987c69736bfb028c45e2981eb9d0b40bd62a353b
Krishna Wijaya <krishnawijaya79@gmail.com>
2023-04-22 12:45:54 +0800

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 const mix = require('laravel-mix')
 
-mix.js('resources/assets/js/app.js', 'publishable/js').vue({ version: 2 })
+mix.js('resources/assets/js/app.js', 'publishable/js').vue({ version: 3 })
