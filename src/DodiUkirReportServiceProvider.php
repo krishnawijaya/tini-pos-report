1a5c2e42e717b7b69ff61c5780187a8c5728e832
Krishna Wijaya <krishnawijaya79@gmail.com>
2023-04-22 09:04:16 +0800

    Update Service Provider

---
+++ src/DodiUkirReportServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Krishnawijaya\DodiUkirReport;
+
+class DodiUkirReportServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        # code...
+    }
+}
