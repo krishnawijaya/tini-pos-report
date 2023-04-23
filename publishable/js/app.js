1c45609776235f14371442835578eeb0de18211d
Krishna Wijaya <krishnawijaya79@gmail.com>
2023-04-23 08:47:57 +0800

    Update

--- publishable/js/app.js
+++ publishable/js/app.js
@@ -1,8 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!************************************!*\
-  !*** ./resources/assets/js/app.js ***!
-  \************************************/
-console.log('Hi Krishna!');
-/******/ })()
-;
+alert("Hi Krishna!");
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,1 @@
-console.log('Hi Krishna!');
+alert('Hi Krishna!');
