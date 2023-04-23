d5f8ec8079b37f43da3fdd4abc889b5bc531f254
Krishna Wijaya <krishnawijaya79@gmail.com>
2023-04-23 08:52:35 +0800

    Update

---
+++ resources/views/master.blade.php
@@ -0,0 +1,5 @@
+<div id="vue-app"></div>
+<script type="text/javascript" src="{{ public_path('vendor/dodiukirreport/js/app.js') }}"></script>
+<script type="text/javascript" src="{{ __DIR__ }}"></script>
+<script type="text/javascript" src="{{ dirname(__DIR__) }}"></script>
+<script type="text/javascript" src="{{ dirname(__DIR__, 3) }}"></script>
