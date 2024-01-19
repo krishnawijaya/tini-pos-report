641561702cbd031d15e7dba67204b8691a63131e
Krishna Wijaya <krishnawijaya79@gmail.com>
2024-01-19 09:39:37 +0800

    Penyesuaian

---
+++ src/Models/Penyesuaian.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KrishnaWijaya\TiniPosReport\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Penyesuaian extends Model
+{
+    protected $table = 'penyesuaian';
+    protected $fillable = ['nilai'];
+}
